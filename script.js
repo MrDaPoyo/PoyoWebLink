621db924a4dca82c8993269e945e37003dc0943d
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-08-08 16:42:29 +0000

    lets ducking go!

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 const JsonUrl = "https://raw.githubusercontent.com/MrDaPoyo/PoyoWebLink/main/ads.json";
-        const url = "https://raw.githubusercontent.com/MrDaPoyo/PoyoWebLink/main/";
-        const media = "media/";
-        
-        async function fetchData() {
-            const response = await fetch(JsonUrl);
-            return await response.json();
-        }
-        
-        async function returnAd() {
-            let ads = await fetchData();
-            ads = ads.ads;
-            let ad = ads[Math.floor(Math.random() * ads.length)];
-            if (ad) {
-                return ad;
-            } else {
-                throw new Error("No ads available");
-            }
-        }
-        
-        async function renderAd() {
-            try {
-                let ad = await returnAd();
-                let adImage = document.createElement("img");
-                adImage.src = media+ad.media;
-                document.body.appendChild(adImage);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        
-        renderAd();
+const url = "https://raw.githubusercontent.com/MrDaPoyo/PoyoWebLink/main/";
+const media = "media/";
+
+async function fetchData() {
+    const response = await fetch(JsonUrl);
+    return await response.json();
+}
+
+async function returnAd() {
+    let ads = await fetchData();
+    ads = ads.ads;
+    let ad = ads[Math.floor(Math.random() * ads.length)];
+    if (ad) {
+        return ad;
+    } else {
+        throw new Error("No ads available");
+    }
+}
+
+async function renderAd() {
+    try {
+        let ad = await returnAd();
+        let adImage = document.createElement("img");
+        adImage.src = media + ad.media;
+        document.body.appendChild(adImage);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+renderAd();
