f90d986a09201a01456cd0c11229edb2e68b3613
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-08-08 15:41:35 +0000

    finished?

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 const JsonUrl = "https://raw.githubusercontent.com/MrDaPoyo/PoyoWebLink/main/ads.json";
+const url = "https://raw.githubusercontent.com/MrDaPoyo/PoyoWebLink/main/";
+const media = "https://github.com/MrDaPoyo/PoyoWebLink/tree/12b0966b0b3bb33a3a5904af38276c47a123c27b/media"
 
 async function fetchData() {
     const response = await fetch(JsonUrl);
-    console.log(await response.json());
+    return await response.json();
 }
 
-fetchData();
+data = fetchData();
+
+
+async function returnAd() {
+    let ads = await fetchData();
+    let ad = ads[Math.floor(Math.random() * ads.length)];
+    return ad;
+}
+
+ad = returnAd();
+console.log(ad);
+
+async function renderAd() {
+    let ad = await returnAd();
+    let adContainer = document.getElementById("adContainer");
+    let adImage = document.createElement("img");
+    adImage.src = media + ad.image;
+    adImage.alt = ad.title;
+    adImage.style.width = "100%";
+    adContainer.appendChild(adImage);
+}
+
+function main()
+{
+    renderAd();
+}
+main();
