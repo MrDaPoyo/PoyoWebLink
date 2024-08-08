95a607d323a327b39bcb326a3f15220c09aa3c39
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-08-08 17:50:22 +0000

    woking

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ async function returnAd() {
     let ads = await fetchData();
     ads = ads.ads;
     let ad = ads[Math.floor(Math.random() * ads.length)];
+    console.log(ads.length);
     if (ad) {
+        console.log();
         return ad;
     } else {
         throw new Error("No ads available");
@@ -21,25 +23,34 @@ async function returnAd() {
 async function renderAd() {
     try {
         let ad = await returnAd();
-        let adImage = document.createElement("img");
-        let adLink = document.createElement("a");
+        let adContainer = document.getElementById("adContainer");
+        let adImage = document.getElementById("adImage");
+        let adLink = document.getElementById("adLink");
+        let adText = document.getElementById("adText");
+        adText.style.position = "absolute";
+        adText.style.bottom = "0";
+        adText.style.left = "0";
+        adText.style.zIndex = "3100";
+        adText.style.width = "128px";
+        adText.style.padding = "0";
+        adText.style.margin = "0";  
+        adText.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        adText.style.color = "white";
         let adNav = document.createElement("a");
         adLink.href = ad.url;
-        adLink.textContent = "";
-        document.body.appendChild(adLink);
+        adLink.style.zIndex = "3090";
+        adLink.style.position = "relative";
+        adContainer.appendChild(adLink);
         adImage.src = media + ad.media;
         adImage.style.width = "128px";
         adImage.style.height = "128px";
         adImage.style.position = "relative";
-        adNav.style.position = "absolute";
-        adNav.style.bottom = "0";
-        adNav.style.left = "0";
-        adNav.style.zIndex = "3090";
-        adNav.textContent = "PoyoWebLink";
         
+        
+        
+
         adLink.appendChild(adImage);
-        adLink.appendChild(adLink);
-        adLink.appendChild(adNav);
+        adImage.appendChild(adNav);
         
     } catch (error) {
         console.error(error);
